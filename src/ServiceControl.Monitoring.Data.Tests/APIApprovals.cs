f81d9e89d95095929070214f95aa706a6d9f8db5
danielmarbach <daniel.marbach@openplace.net>
2021-04-27 16:18:34 +0200

    Update approvals

--- src/ServiceControl.Monitoring.Data.Tests/APIApprovals.cs
+++ src/ServiceControl.Monitoring.Data.Tests/APIApprovals.cs
@@ -10,7 +10,10 @@
         [Test]
         public void Approve()
         {
-            var publicApi = ApiGenerator.GeneratePublicApi(typeof(RingBuffer).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" });
+            var publicApi = typeof(RingBuffer).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+            {
+                ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" }
+            });
             Approver.Verify(publicApi);
         }
     }
